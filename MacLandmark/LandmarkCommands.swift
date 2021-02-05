0a0918ace7ca661848bca0d9a541ced0d23e47c4
lauwarm <10065855+lauwarm@users.noreply.github.com>
2021-02-05 18:51:34 +0100

    add a built-in menu command

---
+++ MacLandmark/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  MacLandmark
+//
+//  Created by Fabian on 05.02.21.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
