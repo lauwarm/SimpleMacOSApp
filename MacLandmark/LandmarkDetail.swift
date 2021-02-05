a8ef87b34dbb8e94a3f963ae324a61e411f0c74a
lauwarm <10065855+lauwarm@users.noreply.github.com>
2021-02-05 18:29:03 +0100

    create a macOS detail view

---
+++ MacLandmark/LandmarkDetail.swift
@@ -0,0 +1,78 @@
+//
+//  LandmarkDetail.swift
+//  MacLandmark
+//
+//  Created by Fabian on 05.02.21.
+//
+
+import SwiftUI
+import MapKit
+
+struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
+    var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+    
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: Alignment(horizontal: .trailing, vertical: .top)) {
+                MapView(coordinate: landmark.locationCoordinate)
+                    .ignoresSafeArea(edges: .top)
+                    .frame(height: 300)
+                
+                Button("Open in Maps") {
+                    let destination = MKMapItem(placemark: MKPlacemark(coordinate: landmark.locationCoordinate))
+                    destination.name = landmark.name
+                    destination.openInMaps()
+                }
+                .padding()
+            }
+
+            VStack(alignment: .leading, spacing: 20) {
+                HStack(spacing: 24) {
+                    CircleImage(image: landmark.image)
+
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text(landmark.name)
+                                .font(.title)
+                                .foregroundColor(.primary)
+                            FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                                .buttonStyle(PlainButtonStyle())
+                        }
+
+                        VStack(alignment: .leading) {
+                            Text(landmark.park)
+                            Text(landmark.state)
+                        }
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                    }
+                }
+
+                Divider()
+
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                Text(landmark.description)
+            }
+            .padding()
+            .frame(maxWidth: 700)
+            .offset(y: -50)
+        }
+        .navigationTitle(landmark.name)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static let modelData = ModelData()
+    
+    static var previews: some View {
+        LandmarkDetail(landmark: modelData.landmarks[0])
+            .environmentObject(modelData)
+            .frame(width: 800, height: 700, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+    }
+}
